32a539051d25596597640282c224f184df308770
1-2meenu <meenakshi20051019@gmail.com>
2026-02-16 09:36:59 +0530

    Create HexadecimalToDecimal.java

---
+++ training/HexadecimalToDecimal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class HexadecimalToDecimal{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        String hex=scan.nextLine().toUpperCase();
+        int decimal=0;
+        int power=0;
+        for(int i=hex.length()-1;i>=0;i--){
+            if(hex.charAt(i)>='0' && hex.charAt(i)<='9'){
+                decimal=decimal+(hex.charAt(i)-'0')*(int)Math.pow(16,power);
+                power++;
+            }
+            else{
+                decimal=decimal+(hex.charAt(i)-'A' +10)*(int)Math.pow(16,power);
+                power++;
+            }
+        }
+        System.out.println(decimal);
+    }
+}
